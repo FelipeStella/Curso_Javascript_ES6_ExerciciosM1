7185124b8bf8d0ff69eedaebf87de2877a51efe8
FelipeStella <felipedasilva.stella@gmail.com>
2020-03-13 15:29:27 -0300

    Exercício #6 e #7

---
+++ exercicio#6#7.js
@@ -0,0 +1,12 @@
+const usuario = 'Diego'; 
+const idade = 23; 
+console.log(`O usuário ${usuario} possui ${idade} anos`);
+
+const name = 'Diego'; 
+const age = 23; 
+const user = { 
+    name,
+    age, 
+    city: 'Rio do Sul', }
+ 
+console.log(user);
